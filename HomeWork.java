0d27ba3f8f74f6c04a6eb44e17a04babbfa27b80
ваш логин на гитхабе < ,    >
2021-11-02 23:21:29 +1200

    lenAndInitialValue

--- HomeWork.java
+++ HomeWork.java
@@ -4,27 +4,36 @@ public class HomeWork {
 
     public static void main(String[] args) {
 
-        //int[] arr1 = new int[]{1, 0, 1, 0, 1, 1, 1};
-        //printBinaryArray(arr1);
+    //    int[] arr1 = new int[]{1, 0, 1, 0, 1, 1, 1};
+    //    printBinaryArray(arr1);
 
-        //printArraySize100(100);
+    //    printArraySize100(100);
 
-        //int[] arr2 = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        //printArrayByTwo(arr2);
+    //    int[] arr2 = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+    //    printArrayByTwo(arr2);
 
-        int [][] arr3 = {
+    /*    int [][] arr3 = {
                 {1, 2, 3, 4, 5},
                 {1, 2, 3, 4, 5},
                 {1, 2, 3, 4, 5},
                 {1, 2, 3, 4, 5},
                 {1, 2, 3, 4, 5},
         };
-        printArray2DForUnit(arr3);
+        printArray2DForUnit(arr3);      */
+
+        lenAndInitialValue(5,25);
 
 
     }
 
-    public static void printArray2DForUnit(int[][] arr) {
+        public static void lenAndInitialValue(int len, int initialValue){
+        int[] arr4 = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr4[i] = initialValue;
+            System.out.print(arr4[i] + " ");
+        }
+    }
+        public static void printArray2DForUnit(int[][] arr) {
         for(int y = 0; y < arr.length; ++y) {
             for(int x = 0, i = arr[y].length; x < arr[y].length; ++x, --i) {
                 if (x == y || y == i - 1) {
@@ -36,8 +45,6 @@ public class HomeWork {
         }
 
     }
-
-
         public static void printArrayByTwo(int[] arr2) {
         for (int i = 0; i < arr2.length; i++) {
             if (arr2[i] < 6){
