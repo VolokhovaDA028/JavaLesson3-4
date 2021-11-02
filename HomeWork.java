3ee36671330b27cbff0f29ab05bc7dbcdb5e193c
ваш логин на гитхабе < ,    >
2021-11-02 22:39:49 +1200

    printBinaryArray&printArraySize100

--- HomeWork.java
+++ HomeWork.java
@@ -3,19 +3,35 @@ package ru.geekbrains.New1.Lesson3;
 public class HomeWork {
 
     public static void main(String[] args) {
-        int[] arr1 = {1, 0, 1, 0, 1, 1, 1};
-        printArray(arr1);
+
+        int[] arr1 = new int[]{1, 0, 1, 0, 1, 1, 1};
+        printBinaryArray(arr1);
+
+        printArraySize100(100);
+        }
+
+        public static void printArraySize100(int size) {
+            int[] arr = new int[size];
+
+            for(int i = 0; i < arr.length; ++i) {
+                arr[i] = i + 1;
+                System.out.print(arr[i] + " ");
+            }
+
+        }
+
+        public static void printBinaryArray(int[] arr1) {
+            for(int i = 0; i < arr1.length; ++i) {
+                if (arr1[i] == 0) {
+                    arr1[i] = 1;
+                } else if (arr1[i] == 1) {
+                    arr1[i] = 0;
+                }
+
+                System.out.print(arr1[i] + " ");
+            }
+
+            System.out.println();
+        }
     }
 
-    public static void printArray(int[] arr1){
-        for (int i = 0; i < arr1.length; i++){
-            if (arr1[i] == 0) {
-                arr1[i] = 1;
-            } else if (arr1[i] == 1){
-                arr1[i] =0;
-            }
-            System.out.print(arr1[i] + " ");
-        }
-        System.out.println("] ");
-    }
-}
