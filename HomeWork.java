91e9e5253d0fbdb49721c274945a8ed2269603bb
ваш логин на гитхабе < ,    >
2021-11-03 12:57:48 +1200

    moveElementArray

--- HomeWork.java
+++ HomeWork.java
@@ -1,6 +1,7 @@
 package ru.geekbrains.New1.Lesson3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class HomeWork {
 
@@ -27,15 +28,57 @@ public class HomeWork {
         //   int[] arr5 = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         //   minMax(arr5);
 
-        int[] arr6 = new int[10];
+        /* int[] arr6 = new int[10];
         for (int i = 0; i < arr6.length; i++) {
             arr6[i] = (int) Math.round((Math.random() * 30) - 15);
             System.out.print(arr6[i] + " ");
         }
-        summArray(arr6);
+        summArray(arr6); */
+
+        //Написать метод, которому на вход подается одномерный массив
+        // и число n (может быть положительным, или отрицательным),
+        // при этом метод должен сместить все элементы массива на n позиций.
+        // Элементы смещаются циклично. Примеры:
+        // [ 1, 2, 3 ] при n = 1 (на один вправо) -> [ 3, 1, 2 ]; [ 3, 5, 6, 1]
+        // при n = -2 (на два влево) -> [ 6, 1, 3, 5 ].
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("На сколько позиций сместить массив?");
+        int n = scanner.nextInt();
+        System.out.println("Масиив будет смещен на " + n + " позиций");
+
+        int[] arr7 = {1, 2, 3, 4, 5, 6, 7};
+        int[] b = new int[arr7.length];
+        moveElementArray(arr7, n, b);
+
+
 
 
     }
+    public static void moveElementArray ( int[] arr7, int n, int[] b) {
+        System.out.print("Исходный массив [ ");
+        for (int i = 0; i < arr7.length; ++i) {
+            System.out.print(arr7[i] + " ");
+        }
+        System.out.println("] ");
+
+        System.out.print("Массив со сдвигом на " + n + " элементов [ ");
+        for (int i = arr7.length-1; i >= 0; i--) {
+            if(i+n >= arr7.length){
+                b[i+n-arr7.length] = arr7[i];
+            }
+            else{
+                b[i+n] = arr7[i];
+            }
+        }
+        for (int i : b) {
+            System.out.print(i + " ");
+        }
+        System.out.println("] ");
+    }
+
+
+
         public static boolean summArray ( int[] arr6){
             int rightSum = 0;
             int leftSum = 0;
